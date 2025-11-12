fa2a8af112626bb8f92ae054ada93cdfe37ffdf9
Hack1ll <gimgagyeong21@gmail.com>
2025-11-13 04:31:28 +0900

    Update coding.c

--- coding.c
+++ coding.c
@@ -1,6 +1,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "file.c"
+
 //1. 파일 읽기 2. 매직넘버 찾기 3. 출력하기
 int main(int argc, char *argv[]){
     
@@ -22,17 +24,9 @@ int main(int argc, char *argv[]){
         printf("reading error");
         exit(1);
     }
-    char signiture[100];
-    if(p[0]==0xff&&p[1]==0xd8&&p[2]==0xff&&p[3]==0xe0||p[0]==0xff&&p[1]==0xd8&&p[2]==0xff&&p[3]==0xe8)strcpy(signiture, "JPEG");
-
-    if(p[0]==0x25&&p[1]==0x50&&p[2]==0x44&&p[3]==0x46&&p[4]==0x2d&&p[5]==0x31&&p[6]==0x2e)strcpy(signiture, "pdf");
+    char signiture=var(p);
     
-    if(p[0]==0x50&&p[1]==0x4b&&p[2]==0x03&&p[3]==0x04){
-        if(p[5]==0x00&&p[6]==0x06&&p[7]==0x00)strcpy(signiture, "docx");
-        else{
-            strcpy(signiture, "zip");
-        }
-    }
+    
     
     printf("%s", signiture);
     return 0;
